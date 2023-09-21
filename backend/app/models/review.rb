b88915981ec2450a27e01fa88d3782112404daa9
Khairul Haaziq <rhycoz@yahoo.com>
2023-09-21 11:32:35 +0800

    fix[backend]: delete campsite cache when review data changes

--- backend/app/models/review.rb
+++ backend/app/models/review.rb
@@ -1,7 +1,14 @@
 class Review < ApplicationRecord
+  after_save :delete_campsite_cache
+  before_destroy :delete_campsite_cache
+
   belongs_to :campsite
   belongs_to :user
 
   validates :body, presence: true
   validates :rating, presence: true
+
+  def delete_campsite_cache
+    Campsite.delete_cache
+  end
 end
